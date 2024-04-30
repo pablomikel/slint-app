ded0711afe33cdacda852e68a1cbfdf2e3371fd8
pablomikel <pablo.olavarr@gmail.com>
2024-04-29 17:46:13 -0700

    fix: stuff

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
+
+
 fn main() {
-    slint_build::compile("ui/appwindow.slint").unwrap();
+
+    let config =
+    slint_build::CompilerConfiguration::new()
+    .with_style("cupertino".into());
+
+    slint_build::compile_with_config("ui/appwindow.slint", config).unwrap();
 }
