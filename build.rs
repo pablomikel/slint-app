5d37517be22617f695e72e39dd3730c6a5d93494
pablomikel <pablo.olavarr@gmail.com>
2024-04-30 20:40:10 -0700

    feat: animation test

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
 
     let config =
     slint_build::CompilerConfiguration::new()
-    .with_style("cupertino".into());
+    .with_style("cupertino-dark".into());
 
     slint_build::compile_with_config("ui/appwindow.slint", config).unwrap();
 }
